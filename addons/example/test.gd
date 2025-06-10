9bdffc78ebf6a6ae1dd9fee73d42bc91f25f5cd4
MelodyTheMuse <105441849+MelodyTheMuse@users.noreply.github.com>
2025-06-10 19:01:25 +0200

    dialog timer added

--- addons/example/test.gd
+++ addons/example/test.gd
@@ -6,6 +6,8 @@ extends Control
 @onready var option_btn_ctnr = $ScrollContainer/OptionButtonContainer
 @onready var next_dialogue_btn = $Button
 var selected_option = ""
+var dia:Control
+var timer:Timer
 
 func _ready() -> void:
 	dialogue_reader.end_reached.connect(on_end_reached)
@@ -39,8 +41,27 @@ func option_pressed(option):
 	for btn in option_btn_ctnr.get_children():
 		btn.queue_free()
 	_on_button_pressed()
-	
+
 func on_end_reached():
 	next_dialogue_btn.hide()
 	end.show()
 	Input.mouse_mode = 2
+	dialog_timer()
+
+func hide_dialog():
+	for c in Game.hud.get_children():
+		if c.name == "Dialogbox":
+			dia = c
+			dia.visible = false
+
+func dialog_timer():
+	if timer:
+		timer.start(1)
+	else:
+		timer = Timer.new()
+		timer.timeout.connect(hide_dialog)
+		timer.one_shot = true
+		timer.autostart = true
+		timer.wait_time = 1
+		add_child(timer)
+	
--- interviewer.gd
+++ interviewer.gd
@@ -6,33 +6,14 @@ var Dialogbox
 
 func start_interview():
 	Input.mouse_mode = 3
-	find_dialog_label()
-	Game.player.dialog_signal.connect(raise_x)
-	choose_dialog_interview()
+	find_dialog()
 	display_dialog_interview()
 
-func find_dialog_label():
+func find_dialog():
 	for c in Game.hud.get_children():
 		if c.name == "Dialogbox":
 			Dialogbox = c
-			label = c.get_child(1)
-
-func choose_dialog_interview():
-	match x:
-		1:
-			text = "Good day to you"
-		2: 
-			text= "Shall we begin?"
-		3:
-			text = "I have read through your CV, and I have to say it is pretty good"
-		4:
-			text = "There is a few things that we have to go through however, before we can start the rest of the hiring process"
-	display_dialog_interview()
+			
 
 func display_dialog_interview():
 	Dialogbox.visible = true
-	label.text = text
-
-func raise_x():
-	x +=1
-	choose_dialog_interview()
